5de513b944cdbffeaf32a3ae29ea74b3c9a3b02e
Ethan Elbert <your-github-email@example.com>
2026-05-21 18:11:32 -0400

    Created fahrenheit to celsius conterter and renamed c to f converter

--- sandbox.py
+++ sandbox.py
@@ -7,5 +7,8 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-def temp_converter(celsius):
+def temp_converter_celsius(celsius):
     return (celsius * 9/5) + 32
+
+def temp_converter_fahrenheit(fahrenheit):
+    return (fahrenheit - 32) * 5/9
